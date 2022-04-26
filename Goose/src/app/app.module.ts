52309f7cd45ac6893cf146724fd0863b0dd6822a
Zeke Wandeler <77249187+zekewandeler@users.noreply.github.com>
2022-04-26 15:39:43 -0600

    WORKING GEOLOCATION

--- Goose/src/app/app.module.ts
+++ Goose/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// geolocation and native-geocoder
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
 // import { GoogleMapsModule } from '@angular/google-maps'
 
 
@@ -14,7 +18,7 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Geolocation, NativeGeocoder, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Goose/src/app/tab2/tab2.page.ts
+++ Goose/src/app/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+
 export class Tab2Page {
-
-  constructor() {}
-
+  latitude: any = 0; //latitude
+  longitude: any = 0; //longitude
+  constructor(
+    private geolocation: Geolocation
+  ) {}
+  options = {
+    timeout: 10000, 
+    enableHighAccuracy: true, 
+    maximumAge: 3600
+  };
+  // use geolocation to get user's device coordinates
+  getCurrentCoordinates() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
 }
